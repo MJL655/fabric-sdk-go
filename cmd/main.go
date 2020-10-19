dad3444231294b7f991ab72bb2cbeba8663431a8
studyzy <studyzy@gmail.com>
2020-10-19 16:56:51 +0800

    add main.go for test

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/ledger"
+	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+)
+func main() {
+	queryChainInfo()
+}
+var (
+	fabricSdk        *fabsdk.FabricSDK
+userName string
+	orgName string
+)
+
+func queryChainInfo() error {
+
+	channelContext := fabricSdk.ChannelContext("mychannel", fabsdk.WithUser(userName), fabsdk.WithOrg(orgName))
+	client, err := ledger.New(channelContext)
+	if err != nil {
+		return err
+	}
+	result, err := client.QueryInfo()
+	if err != nil {
+		log.Fatal(err.Error())
+		return err
+	} else {
+		height := result.BCI.Height
+		blockHash := hex.EncodeToString(result.BCI.CurrentBlockHash)
+		if err != nil {
+			return err
+		}
+		fmt.Println("block_number", height)
+		fmt.Println("current_block_hash", blockHash)
+	}
+	return nil
+}
